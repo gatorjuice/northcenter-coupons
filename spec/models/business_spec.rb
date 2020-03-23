280337ec8822a8dfe1bb347c47e0c482da46a8b1
James Gates <gatorjuice@gmail.com>
2020-03-22 22:22:40 -0500

    added test for scope method

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Business, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.search' do
+    let!(:business_b) { create(:business, :with_coupons, name: 'Pizza by Alfredo') }
+    let!(:business_a) { create(:business, :with_coupons, name: 'Piazza Del Tormini') }
+    let!(:business_c) { create(:business, :with_coupons, name: 'Duley Dentistry') }
+
+    let(:search_param) { 'zZa' }
+
+    it 'returns the matching businesses' do
+      expect(Business.search(search_param)).to eq([business_a, business_b])
+    end
+  end
 end
