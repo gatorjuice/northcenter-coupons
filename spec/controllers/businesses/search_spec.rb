12772e4c6c21222836e4c27f0da001cd07482957
James Gates <gatorjuice@gmail.com>
2020-03-22 21:57:19 -0500

    test refactor

--- spec/controllers/businesses/search_spec.rb
+++ spec/controllers/businesses/search_spec.rb
@@ -3,18 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe BusinessesController, type: :controller do
-  include Devise::Test::ControllerHelpers
-
   describe 'GET #search' do
-    subject(:search) { get :search, params: { q: 'zZa' } }
+    subject(:search) { get :search, params: params }
 
     let!(:business_b) { create(:business, :with_coupons, name: 'Pizza by Alfredo') }
     let!(:business_a) { create(:business, :with_coupons, name: 'Piazza Del Tormini') }
     let!(:business_c) { create(:business, :with_coupons, name: 'Duley Dentistry') }
 
-    before do
-      search
-    end
+    let(:params) { { q: 'zZa' } }
+
+    before { search }
 
     it 'assigns @businesses in the correct order' do
       expect(assigns(:businesses)).to eq([business_a, business_b])
@@ -25,7 +23,7 @@ RSpec.describe BusinessesController, type: :controller do
     end
 
     context 'when the query string is empty' do
-      subject(:search) { get :search, params: { q: '' } }
+      let(:params) { { q: '' } }
 
       it 'redirects to the index action if the query is an empty string' do
         expect(response).to render_template(:index)
